f377fc26084c3dbd08d76e782f83ea84a0685ba5
Zijian ZHONG <593958169@qq.com>
2021-12-23 19:13:58 +0800

    Started to do homework...

---
+++ BALio.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 23 19:07:35 2021
+
+@author: Zijian Zhong
+
+This script is to read data from BAL dataset
+"""
+
+def read_bal(filename):
+    camera_index = [] # The camera index in each observation
+    point_index = [] # The point index in each observation
+    cameras = [] # 
+    points = []
+    
+
